3283bd22fef5076a21d968f20940b437499af5e0
svpcadmin <PhilisPaxil@gmail.com>
2020-08-18 17:43:36 +0900

    add: eslint

--- index.js
+++ index.js
@@ -19,34 +19,49 @@ fs.copy(path.join(__dirname, "./fe/tsconfig.json"), "./tsconfig.json").catch(
   }
 );
 
-const modules =  [
-  "typescript",
+const modules = [
   "react-dom",
   "react",
   "@types/react",
   "@types/react-dom",
+  "ts-loader",
+  "babel-loader",
+  "html-webpack-plugin",
+  "react-router-dom",
+  "mini-css-extract-plugin",
+  "react-router-dom",
+  "@types/react-router-dom",
+];
+
+const modulesDev = [
+  "typescript",
+  "prettier",
+  "eslint",
+  "eslint-plugin-import",
+  "eslint-plugin-jsx-a11y",
+  "eslint-plugin-react",
+  "eslint-plugin-react-hooks",
+  "@types/jest",
+  "@babel/preset-env",
   "jest",
   "ts-jest",
   "webpack",
   "webpack-cli",
   "webpack-dev-server",
-  "ts-loader",
-  "babel-loader",
-  "html-webpack-plugin",
   "dotenv",
-  "react-router-dom",
-  "@babel/preset-env",
-  "mini-css-extract-plugin",
-  "@types/jest",
-  "react-router-dom",
-  "@types/react-router-dom",
-]
+  "eslint-config-prettier",
+  "eslint-plugin-prettier",
+];
 
-var install = require('spawn-npm-install')
- 
-install(modules, { saveDev: false }, function(err) {
-  if (err)
-    console.error("Could not install:\n" + err.message)
-  else
-    console.log("Installed.")
-})
+const install = require("spawn-npm-install");
+
+install(modules, { saveDev: false }, function (err) {
+  if (err) console.error("Could not install:\n" + err.message);
+  else {
+    console.log("Modules are Installed.");
+    install(modulesDev, { saveDev: true }, (err2) => {
+      if (err2) console.error("cloud not install");
+      else console.log("done");
+    });
+  }
+});
