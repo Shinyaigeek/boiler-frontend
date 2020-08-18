a48113899352e2ddae4eda6bc3c3367634bbdee1
svpcadmin <PhilisPaxil@gmail.com>
2020-08-18 16:19:01 +0900

    add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# boiler-frontend
+
+This is boilerplate for my frontend development
+
+## what includes
+
+* typescript
+* react
+* react-router-dom
+* styled-jsx
+* webpack
--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 const fs = require("fs-extra");
 const path = require("path");
+const { spawn } = require("child_process");
 
-fs.copyFile(path.join(__dirname, "./fe"), "./");
+fs.copy(path.join(__dirname, "./fe/src"), "./src").catch((e) => {
+  throw new Error(e);
+});
+
+fs.copy(
+  path.join(__dirname, "./fe/webpack.config.js"),
+  "./webpack.config.js"
+).catch((e) => {
+  throw new Error(e);
+});
+
+fs.copy(path.join(__dirname, "./fe/tsconfig.json"), "./tsconfig.json").catch(
+  (e) => {
+    throw new Error(e);
+  }
+);
+
+const modules =  [
+  "typescript",
+  "react-dom",
+  "react",
+  "@types/react",
+  "@types/react-dom",
+  "jest",
+  "ts-jest",
+  "webpack",
+  "webpack-cli",
+  "webpack-dev-server",
+  "ts-loader",
+  "babel-loader",
+  "html-webpack-plugin",
+  "dotenv",
+  "react-router-dom",
+  "@babel/preset-env",
+  "mini-css-extract-plugin",
+  "@types/jest",
+  "react-router-dom",
+  "@types/react-router-dom",
+]
+
+var install = require('spawn-npm-install')
+ 
+install(modules, { saveDev: false }, function(err) {
+  if (err)
+    console.error("Could not install:\n" + err.message)
+  else
+    console.log("Installed.")
+})
